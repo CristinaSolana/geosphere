716722ab0090e85159412a49992844d081c0765a
Cristina Solana <cs@nightshiftcreative.com>
2017-12-29 20:49:28 -0500

    feat: movement with mouse down

--- Assets/test.cs
+++ Assets/test.cs
@@ -4,41 +4,33 @@ using UnityEngine;
 
 
 public class test : MonoBehaviour {
- 	
-	public GameObject target;
-    private float speedMod = 5.0f;
+
+	private GameObject target;
+    private float speed = 10.0f;
     private Vector3 point;
-   
+    private float moveX;
+    private float moveY;
+
     void Start () {
 
-		target = GameObject.Find("Sphere");
+		target = GameObject.Find("GeoSphere");
         point = target.transform.position;
         transform.LookAt(point);
 
     }
-   
+    
     void Update () {
 
-		// if (Input.GetMouseButton(0))
-        //     transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
+		if (Input.GetMouseButton(0)) {
 
-		if (Input.GetMouseButtonDown(0))
-			Debug.Log(Input.mousePosition);
+            moveX = Input.GetAxis("Mouse X");
+            moveY = Input.GetAxis("Mouse Y");
 
-		if (Input.GetMouseButtonUp(0))
-			Debug.Log(Input.mousePosition);
+            Vector3 movement = new Vector3(0f,moveX,moveY);
+            
+            transform.RotateAround (point,movement,20 * Time.deltaTime * speed);
 
-		if (Input.GetKey("up"))
-            transform.RotateAround (point,new Vector3(0.0f,0.0f,-1.0f),20 * Time.deltaTime * speedMod);
-        
-        if (Input.GetKey("down"))
-            transform.RotateAround (point,new Vector3(0.0f,0.0f,1.0f),20 * Time.deltaTime * speedMod);
-
-		if (Input.GetKey("left"))
-            transform.RotateAround (point,new Vector3(0.0f,-1.0f,0.0f),20 * Time.deltaTime * speedMod);
-        
-        if (Input.GetKey("right"))
-            transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
+        }
 
     }
 
